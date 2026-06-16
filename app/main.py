87a96e6978d90a707c6f90e43f8bc0fcb7519dbc
Yuri Hayakawa <yuriteixeira1108@gmail.com>
2026-06-16 16:12:29 -0300

    feat: create user model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.database import Base, engine
+from app.models import User
 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
